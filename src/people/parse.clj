63fea8fbcfb5fc5e5c679791c66f8519889cd5d7
Craig Ludington <me@alpheus.me>
2015-08-06 15:44:15 -0500

    Add parser for pipe, comma, or space delimited records.

---
+++ src/people/parse.clj
@@ -0,0 +1,33 @@
+(ns people.parse
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(defn- infer-delimiter
+  "Infer the input delimiter by scanning input for one of the known delimiters."
+  [input]
+  {:pre [(string? input)]}
+  (cond (re-find #"\|" input) \|
+        (re-find #","  input) \,
+        :else \space))
+
+(defn- make-parser
+  "Return an appropriate parser for the given input string."
+  [input]
+  {:pre [(string? input)]}
+  (partial csv/read-csv input :separator (infer-delimiter input)))
+
+  {:pre [(string? input)]}  ;; java.io.Reader
+
+(defmulti parse
+  "Parse input consisting of newline-terminated records with fields delimited by a pipe, comma, or space.
+   Return a collection of records -- each record is a vector of strings, one per field."
+  [input]
+  class)
+
+(defmethod parse String
+  [input]
+  (doall ((make-parser input))))
+
+(defmethod parse java.io.Reader
+  [input]
+  (parse (slurp input)))

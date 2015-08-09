d0926e5f89cd05b65fc1f3484e15b3a33072219d
Craig Ludington <me@alpheus.me>
2015-08-08 22:23:11 -0500

    Add ring.middleware.format for wrap-restful-format
    * Converts Clojure datastructures in the body of a response to JSON

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [ring-middleware-format "0.5.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [clj-time "0.10.0"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
--- src/people/handler.clj
+++ src/people/handler.clj
@@ -1,11 +1,15 @@
 (ns people.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.format :refer [wrap-restful-format]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hello World!!!")
+  (GET "/json" [] {:body {:msg "Hi there!"}})
   (route/not-found "Not Found"))
 
+;;  
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (wrap-defaults app-routes site-defaults)
+      (wrap-restful-format :formats [:json-kw])))
